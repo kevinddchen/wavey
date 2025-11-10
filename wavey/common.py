496cde999411fa6e2cf136c3964ad85325b6b1a7
Kevin Chen <79341521+kevinddchen@users.noreply.github.com>
2025-11-09 22:45:14 -0800

    .

--- wavey/common.py
+++ wavey/common.py
@@ -9,7 +9,7 @@ DATETIME_FORMAT = "%a %b %d %H:%M (Pacific)"
 FEET_PER_METER = 3.28
 
 # Lat/lon bounding box for zoom-in on Monterey peninsula
-LAT_MIN = 36.4  # 36.2
-LAT_MAX = 36.7  # 37.0
-LON_MIN = 237.9  # 237.8
-LON_MAX = 238.2  # 238.3
+LAT_MIN = 36.4
+LAT_MAX = 36.7
+LON_MIN = 237.9
+LON_MAX = 238.2
