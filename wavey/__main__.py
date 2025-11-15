975be455468f55f12140490d97fa8d5ce0e9fc7e
Kevin Chen <79341521+kevinddchen@users.noreply.github.com>
2025-11-14 22:32:14 -0800

    use fixed png palette

--- wavey/__main__.py
+++ wavey/__main__.py
@@ -60,7 +60,7 @@ def savefig(path: Path) -> None:
     plt.savefig(bts, format="png")
 
     with PIL.Image.open(bts) as img:
-        img2 = img.convert("RGB").convert("P", palette=PIL.Image.Palette.ADAPTIVE)
+        img2 = img.convert("RGB").convert("P", palette=PIL.Image.Palette.WEB)
         img2.save(path, format="png")
 
 
